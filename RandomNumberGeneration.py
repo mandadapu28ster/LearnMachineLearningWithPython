7dd72c9908b2dd9152a661b2b7a91eb3aff22cad
mandadapu <mandadapu28ster@gmail.com>
2018-08-15 16:34:02 +0100

    Rao - Update pending

--- RandomNumberGeneration.py
+++ RandomNumberGeneration.py
@@ -1,20 +1,20 @@
 from random import seed
 from random import randint
-
+import numpy
 from numpy import prod
 
-seed(1)
+seed(3)
 X, y = list(), list()
-for i in range(100):
+for i in range(10):
 	in_pattern = [randint(1,100) for _ in range(2)]
-	out_pattern = prod(in_pattern)
+	out_pattern = sum(in_pattern)
 	print(in_pattern, out_pattern)
 	# X.append(in_pattern)
 	# y.append(out_pattern)
 
-# # load pima indians dataset from local file
-# dataset = numpy.loadtxt("pima-indians-diabetes.data.csv", delimiter=",")
-# # split into input (X) and output (Y) variables
-# X = dataset[:,0:8]
-# Y = dataset[:,8]
+# load pima indians dataset from local file
+# dataset = numpy.loadtxt("AdditionData.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+# X = dataset[:,0:2]
+# Y = dataset[:,2]
 # print(X, Y)
